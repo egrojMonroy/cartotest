70ff152e5eb987485b0fcd9443aab43a08034e4e
Lua <xdaddisxd@gmail.com>
2022-04-07 12:32:32 -0400

    asdfasdf

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ function App() {
       iconAtlas: 'https://s3.amazonaws.com/com.cartodb.users-assets.production/maki-icons/marker-stroked-24.svg?req=markup'
     });
     console.log(cartLayer);
-  }, [ICON_MAPPING]);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
